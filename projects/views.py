795f8f5c50cc4d90e6a80e48f3eece9f460f2933
Anurag Yadav <55681370+anuragyadav19150@users.noreply.github.com>
2022-12-29 22:36:50 +0530

    changes done

--- projects/views.py
+++ projects/views.py
@@ -34,41 +34,62 @@ def index(request):
     # print("sending")
     data = {
         "projectInfo": {
-            "2": {
+            "1": {
                 "firstLine": "Microsoft Teams Engage",
                 "description": "I made this Microsoft Teams Clone App in Microsoft Engage 2021.You can download and run this app.",
                 "github": "https://github.com/anuragyadav19150/ms_engage_2021",
-                "image": "NA"
+                "image": "../static/projects/ms.jpg",
+                "techStack":"Futter , Dart , Android Studio , Agora video service",
+                "descriptionNew": "I made this Microsoft Teams Clone App in Microsoft Engage 2021.You can download and run this app. Create an Account and Obtain an App ID To build and run the sample application, first obtain an app ID: Create a developer account at agora.io. Once you finish the sign-up process, you are redirected to the dashboard. Navigate in the dashboard tree on the left to Projects > Project List. Copy the app ID that you obtain from the dashboard into a text file. You will use this when you launch the app.",
+ 
             },
             "2": {
                 "firstLine": "Shopify - Full stack",
                 "description": "Designed a system that maintains the database of a Shopping website along with its\r\nFully Functional website.",
                 "github": "https://github.com/anuragyadav19150/Shopify_DBMS",
-                "image": "NA"
+                "image": "../static/projects/shop.png",
+                "techStack":"React , Node , MySql ",
+                "descriptionNew": "Designed a system that maintains the database of a Shopping website along with its\r\nFully Functional website. We ahave added many features like Login , signup and admin control where admin can control every thing fromuser info to product infomation.",
+                
+
             },
             "3": {
                 "firstLine": "Traffic Rule Violation Detection",
                 "description": "We detect traffic violations like over-speeding and crossing the Red light Signal using\r\ncomputer vision and Image processing.",
                 "github": "https://github.com/anuragyadav19150/Traffic-Rule-Violation-detection",
-                "image": "NA"
+               "image": "../static/projects/ms.png",
+                "techStack":"Python , computer vision",
+                "descriptionNew": "a traffic violation system using Computer vision is highly needed as these systems can act as an efficient tool to reduce traffic violations and penalise violators. So in this project, we are trying to develop a computer vision model that can detect violations of basic traffic rules like not wearing a helmet, overspeeding, and jumping red lights",
+                
+
             },
             "4": {
                 "firstLine": "Color Switch",
-                "description": "Made a clone of the famous game Color Switch with JavaFX in JAVA",
+                "description": "Made a clone of the famous game Color Switch with JavaFX in JAVA.",
                 "github": "https://github.com/anuragyadav19150/apFinal",
-                "image": "NA"
+                "image": "../static/projects/ms.png",
+                "techStack":"Java , Java FX",
+                "descriptionNew": "Made a clone of the famous game Color Switch with JavaFX in JAVA. We have used many oop's features to make this project, We also have made the uml diagram so that we can plan how we are going to do this project and also a use case diagram so that we have a clear picture of the final product in our mind hope you like it!",
+                
+
             },
             "5": {
                 "firstLine": "Dream Team Creator",
                 "description": "We made a model that will make a team which will win the tournament by using the\r\ndata present online",
                 "github": "https://github.com/anuragyadav19150/Machine_Learning_Final_Project",
-                "image": "NA"
+                "image": "../static/projects/ms.png",
+                "techStack":"Pyhton , Machine Learning ",
+                "descriptionNew": "The main focus of this project is to create a winning fantasy team by looking at the huge amount of data present online and predicting which team will win by analyzing player’s performance history, statistics against the opponent team, home ground status, weather impact, and many other factors and attempt to predict the outcome",
+                
             },
             "6": {
                 "firstLine": "GRAM VISION",
                 "description": "Developing a user-friendly, Easy to use Mobile App, using Flutter, Dart, and\r\nGoogle Firebase(for backend), for the people of a village.",
                 "github": "https://github.com/anuragyadav19150/GramVision",
-                "image": "NA"
+                "image": "../static/projects/ms.png",
+                "techStack":"Futter , Dart , Android Studio , Google fire base",
+                "descriptionNew": "Developing a user-friendly, Easy to use Mobile App, using Flutter, Dart, and\r\nGoogle Firebase(for backend), for the people of a village. Made an app Panchayat using Flutter, Dart and Google Firebase. It is centered around people of villages where they can get info about latest announcements & policies, file personal and general complaints, sell/buy products and contact buyer/seller using the inbuilt messaging app and many other features.",
+ 
             }
         },
         "skills": {
